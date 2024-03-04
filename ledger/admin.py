50378710df6afc0fb346d5827c6466760f69f3c1
Hans Zhuang <157770734+hans-zhuang@users.noreply.github.com>
2024-03-04 23:34:08 +0800

    Revert "lab2"

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,14 +1,3 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
 
 # Register your models here.
-
-class RecipeIngredientLine(admin.TabularInline):
-    model = RecipeIngredient
-
-class RecipeAdmin(admin.ModelAdmin):
-    model = [RecipeIngredientLine]
-
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+
+from .views import recipe_list, recipe_1, recipe_2
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipes/list', recipe_list, name='list'),
+    path('recipe/1', recipe_1, name='1'),
+    path('recipe/2', recipe_2, name='2'),
 ]
+
 app_name = "ledger"
